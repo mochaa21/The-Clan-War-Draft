879029b005c5c72501e2a11235c81d3c48aaab77
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-07 21:52:45 +0700

    feat: task

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import List
+
+clan_members = [
+    "  Zane | TH11 | 45 | 2800",
+    "Ariel | TH11 | 50 | 2900 ",
+    "Bob | TH10 | 40 | 2500",
+    "  Charlie | TH11 | 45 | 2800  "
+]
+
+# TUGAS: Buat fungsi draft_selection(members) dari nol di sini.
+# Pastikan lu membersihkan spasinya, membuang tulisan "TH" agar bisa jadi integer, 
+# dan menyusun tuple 4 dimensinya dengan benar.
+
+
+# --- EKSEKUSI ---
+# hasil_draft = draft_selection(clan_members)
+# for member in hasil_draft:
+#     print(member.strip())
